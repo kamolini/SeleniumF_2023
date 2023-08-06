e012f9482686d5eb8b32d7a41dab701a92d1c8ee
kamolesh <kamoleshbachar@gmail.com>
2023-08-06 12:19:34 -0400

    Added new code

---
+++ src/test/java/r/P.java
@@ -0,0 +1,56 @@
+package r;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import olr.Bachar;
+import olr.Kamolesh;
+
+public class P {
+	
+	
+	public double sum() {
+		
+		return 2.2;
+	}
+	
+	public double[] sum(double a, double b) {
+
+		
+		return new double[5];
+	}
+	
+	public List<Double> sum(double a) {
+
+		
+		return new ArrayList<Double>();
+	}
+	
+	public Bachar k(double a) {
+
+		
+		return new Kamolesh();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
