0184857305ea8e28a6125fd492529da8feb09434
kamolesh <kamoleshbachar@gmail.com>
2023-11-05 11:01:43 -0500

    Changed config

---
+++ src/test/java/test/ui/Li.java
@@ -0,0 +1,23 @@
+package test.ui;
+
+public class Li {
+	
+	
+	public static void main(String[] args) {
+		
+		boolean fS = true;
+		boolean sS = false;
+		
+		if(fS == sS) {
+			System.out.println("On");
+		}else{
+			System.out.println("Off");
+		}
+		
+		
+	}
+	
+	
+	
+
+}
